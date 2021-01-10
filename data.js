52f47d750ed8de7a106ea7f34f15a101036d8ca1
Iri04 <ira18da017@g.sugiyama-u.ac.jp>
2021-01-10 20:18:14 +0900

    マップに表示
    マップにすべてのマーカーを表示しました。

---
+++ data.js
@@ -0,0 +1,704 @@
+data={
+  "type":"FeatureCollection",
+  "features":[
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.432559,34.723624]
+      },
+      "properties":
+      {
+        "ID":"1",
+        "店舗名":"ラウンドワンスタジアム　豊橋店",
+        "郵便番号":"441-3147",
+        "住所":"愛知県豊橋市大岩町字岩田27番地-2（国道１号線沿い）",
+        "電話番号":"0532-65-2212",
+        "営業時間開店":"月曜日〜木曜日　10:00　金曜日・祝前日　10:00　土曜日　24時間営業",
+        "営業時間閉店":"月曜日〜木曜日 6:00　金曜日・祝前日 終日　日曜日・祝日 6:00",
+        "Webサイト":"https://www.round1.co.jp/shop/tenpo/aichi-toyohashi.html",
+        "Twitter":"https://twitter.com/r1_toyohashi"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.001409,35.005474]
+      },
+      "properties":
+      {
+        "ID":"2",
+        "店舗名":"M's PARK刈谷店",
+        "郵便番号":"448-0038",
+        "住所":"愛知県刈谷市稲場町3-206",
+        "電話番号":"0566-27-5822",
+        "営業時間開店":"全日　10:00",
+        "営業時間閉店":"全日　24:00",
+        "Webサイト":"https://msp-kariya.amebaownd.com/pages/1913809/InstallationGames",
+        "Twitter":"https://twitter.com/msparkkariya"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.017758,35.120803]
+      },
+      "properties":
+      {
+        "ID":"3",
+        "店舗名":"コムテックプラザ赤池",
+        "郵便番号":"470-0125",
+        "住所":"愛知県日進市赤池1-2901",
+        "電話番号":"052-807-2666",
+        "営業時間開店":"平日　12:00　土休日　10:00",
+        "営業時間閉店":"平日　24:00　土休日　24:00",
+        "Webサイト":"",
+        "Twitter":"https://twitter.com/comtechplaza?lang=ja"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.941735,34.885422]
+      },
+      "properties":
+      {
+        "ID":"4",
+        "店舗名":"ラウンドワンスタジアム 半田店",
+        "郵便番号":"475-0828",
+        "住所":"愛知県半田市瑞穂町6丁目7番地の8",
+        "電話番号":"0569-25-7351",
+        "営業時間開店":"月曜日〜木曜日　10:00　金曜日・祝前日　10:00　土曜日 24時間営業",
+        "営業時間閉店":"月曜日〜木曜日　2:00　金曜日・祝前日　終日　日曜日・祝日　翌朝2:00",
+        "Webサイト":"https://www.round1.co.jp/shop/tenpo/aichi-handa.html",
+        "Twitter":"https://twitter.com/r1_handa"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.92179,34.874775]
+      },
+      "properties":
+      {
+        "ID":"5",
+        "店舗名":"半田メトロポリス",
+        "郵便番号":"475-0838",
+        "住所":"愛知県半田市旭町3-11-1","電話番号":"0569-24-5226",
+        "営業時間開店":"全日　10:00",
+        "営業時間閉店":"全日　23:00",
+        "Webサイト":"http://www.korona.co.jp/Game/han/index.asp",
+        "Twitter":""
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.911613,35.304255]
+      },
+      "properties":
+      {
+        "ID":"6",
+        "店舗名":"Ｍ’ｓ　ＰＡＲＫ　小牧店",
+        "郵便番号":"485-0082",
+        "住所":"愛知県小牧市村中字稲荷783番地",
+        "電話番号":"0568-77-8752",
+        "営業時間開店":"全日　9:00",
+        "営業時間閉店":"全日　24:00",
+        "Webサイト":"",
+        "Twitter":"https://twitter.com/MsPARK_KOMAKI"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.816542,35.310976]
+      },
+      "properties":
+      {
+        "ID":"7",
+        "店舗名":"namco　テラスウォーク一宮店",
+        "郵便番号":"491-0022",
+        "住所":"愛知県一宮市両郷町一丁目2番地",
+        "電話番号":"0586-26-1005",
+        "営業時間開店":"全日　9:00",
+        "営業時間閉店":"全日　21:00",
+        "Webサイト":"https://bandainamco-am.co.jp/game_center/loc/ichinomiya/",
+        "Twitter":""
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.84372,35.254848]
+      },
+      "properties":
+      {
+        "ID":"8",
+        "店舗名":"ラウンドワンスタジアム 名古屋西春店",
+        "郵便番号":"481-0035",
+        "住所":"愛知県北名古屋市宇福寺神明15番地",
+        "電話番号":"0568-26-4001",
+        "営業時間開店":"月曜日　8:00　火曜日～木曜日　10:00　金曜日・祝前日　10:00　土曜日　24時間営業",
+        "営業時間閉店":"月曜日　6:00　火曜日～木曜日　6:00　金曜日・祝前日　終日　日曜日・祝日　6:00",
+        "Webサイト":"https://www.round1.co.jp/shop/tenpo/aichi-nagoyanisiharu.html",
+        "Twitter":"https://twitter.com/r1_nisiharu"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.160046,34.973636]
+      },
+      "properties":
+      {
+        "ID":"9",
+        "店舗名":"ゲームファンタジアン岡崎店",
+        "郵便番号":"444-0067",
+        "住所":"愛知県岡崎市錦町2番地4",
+        "電話番号":"0564-66-0505",
+        "営業時間開店":"全日　9:00",
+        "営業時間閉店":"全日　24:00",
+        "Webサイト":"http://www.fantasian.co.jp/shop5.html",
+        "Twitter":"https://twitter.com/Amex_gfo"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.140439,35.056707]
+      },
+      "properties":
+      {
+        "ID":"10",
+        "店舗名":"M's PARK 豊田店",
+        "郵便番号":"471-0834",
+        "住所":"愛知県豊田市寿町3丁目21",
+        "電話番号":"0565-71-0027",
+        "営業時間開店":"全日　9:00",
+        "営業時間閉店":"全日　24:00",
+        "Webサイト":"",
+        "Twitter":"https://twitter.com/msparktoyota"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[137.211258,34.829001]
+      },
+      "properties":
+      {
+        "ID":"11",
+        "店舗名":"ゲームファンタジアン蒲郡店",
+        "郵便番号":"443-0046",
+        "住所":"愛知県蒲郡市竹谷町中野1-12",
+        "電話番号":"0533-69-8880",
+        "営業時間開店":"全日　9:00",
+        "営業時間閉店":"全日　24:00",
+        "Webサイト":"http://www.fantasian.co.jp/shop3.html",
+        "Twitter":"https://twitter.com/Amex_GFG"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":
+      {
+        "type":"Point",
+        "coordinates":[136.905565,35.159211]
+      },
+      "properties":
+      {
+        "ID":"12",
+        "店舗名":"タイトーステーション フェドラ大須店",
+        "郵便番号":"460-0011",
+        "住所":"愛知県名古屋市中区大須3丁目30-31萬松寺駐車場ビル１階･2階",
+        "電話番号":"052-242-2011",
+        "営業時間開店":"平日　10:00　金曜日　10:00　土曜日　9:00　日曜日　9:00",
+        "営業時間閉店":"平日　23:30　金曜日　24:00　土曜日　24:00　日曜日　23:30",
+        "Webサイト":"https://www.taito.co.jp/store/00002180",
+        "Twitter":"https://twitter.com/Taito_phoe_osu"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.887511,35.007289]
+        },
+        "properties":
+        {
+          "ID":"13",
+          "店舗名":"FGLアトランティス",
+          "郵便番号":"477-0034",
+          "住所":"愛知県東海市養父町3丁目38",
+          "電話番号":"0562-33-8355",
+          "営業時間開店":"平日　11:00　土日　10:00",
+          "営業時間閉店":"平日　24:00　土日　24:00",
+          "Webサイト":"",
+          "Twitter":"https://twitter.com/atlantis8355"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.905552,35.296036]
+        },
+        "properties":
+        {
+          "ID":"14",
+          "店舗名":"小牧メトロポリス",
+          "郵便番号":"485-0015",
+          "住所":"愛知県小牧市間々片山181",
+          "電話番号":"0568-71-0005",
+          "営業時間開店":"平日　10:00　土日祝　9:00",
+          "営業時間閉店":"平日　23:50　土日祝　23:50",
+          "Webサイト":"https://www.korona.co.jp/Game/kom/index.asp",
+          "Twitter":"https://twitter.com/metropolis_kmk"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.876602,35.173083]
+        },
+        "properties":
+        {
+          "ID":"15",
+          "店舗名":"GAME.BOX Q3",
+          "郵便番号":"453-0014",
+          "住所":"愛知県名古屋市中村区則武１丁目16-8",
+          "電話番号":"052-452-0920",
+          "営業時間開店":"平日　10:00　土日祝　9:00",
+          "営業時間閉店":"平日　24:00　土日祝　24:00",
+          "Webサイト":"",
+          "Twitter":"https://twitter.com/9m5w0woxsun995w"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.885992,35.1694]
+        },
+        "properties":
+        {
+          "ID":"16",
+          "店舗名":"コムテックタワー",
+          "郵便番号":"450-0002",
+          "住所":"愛知県名古屋市名駅4丁目10-20",
+          "電話番号":"052-565-6471",
+          "営業時間開店":"平日　10:00　休日　9:00",
+          "営業時間閉店":"平日　24:00　休日　24:00",
+          "Webサイト":"http://comtechtower.web.fc2.com/",
+          "Twitter":"https://twitter.com/ComtechTower"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.959884,35.269455]
+          },
+          "properties":
+          {
+            "ID":"17",
+            "店舗名":"セガ春日井",
+            "郵便番号":"486-0805",
+            "住所":"愛知県春日井市岩野町4180",
+            "電話番号":"0568-89-3081",
+            "営業時間開店":"平日　10:00　土日祝　9:00",
+            "営業時間閉店":"平日　24:00　土日祝　24:00",
+            "Webサイト":"https://tempo.sega.jp/am/kasugai",
+            "Twitter":"https://twitter.com/SEGA_kasugai"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.900001,35.144021]
+          },
+          "properties":
+          {
+            "ID":"18",
+            "店舗名":"コムテックスクウェア",
+            "郵便番号":"460-0022",
+            "住所":"愛知県名古屋市中区金山1丁目16-8",
+            "電話番号":"052-321-5601",
+            "営業時間開店":"全日　9:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"",
+            "Twitter":"https://twitter.com/comtechsquare"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[137.06294,35.209151]
+          },
+          "properties":
+          {
+            "ID":"19",
+            "店舗名":"セガ東名ボール",
+            "郵便番号":"489-0972",
+            "住所":"愛知県瀬戸市西原町2丁目114",
+            "電話番号":"0561-85-6211",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"https://tempo.sega.jp/am/tomei-bowl",
+            "Twitter":"https://twitter.com/SEGA_tomei_bowl"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.908993,35.226407]
+          },
+          "properties":
+          {
+            "ID":"20",
+            "店舗名":"ファンタジープラザ名古屋店",
+            "郵便番号":"462-0018",
+            "住所":"愛知県名古屋市北区玄馬町234-1",
+            "電話番号":"052-938-3461",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　5:00",
+            "Webサイト":"https://www.donki.com/store/shop_tenant.php?shop_id=163&tenant_id=2226",
+            "Twitter":"https://twitter.com/fpnagoya"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.848794,35.239859]
+          },
+          "properties":
+          {
+            "ID":"21",
+            "店舗名":"楽市楽座022　楽市街道名古屋店",
+            "郵便番号":"481-0045",
+            "住所":"愛知県北名古屋市中之郷神明45番1",
+            "電話番号":"0568-27-2622",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"https://rakupa.jp/shop/022rakuichikaido-nagoya/",
+            "Twitter":"https://twitter.com/nagoyagaidou"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[137.371803,34.744201]
+          },
+          "properties":
+          {
+            "ID":"22",
+            "店舗名":"タイトーステーション豊橋店",
+            "郵便番号":"441-8061",
+            "住所":"愛知県豊橋市藤沢町141-4",
+            "電話番号":"0532-29-8511",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"https://www.taito.co.jp/store/00001936",
+            "Twitter":"https://twitter.com/Taito_Toyohashi"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[137.123689,35.067913]
+          },
+          "properties":
+          {
+            "ID":"23",
+            "店舗名":"セガ豊田",
+            "郵便番号":"471-0841",
+            "住所":"愛知県豊田市深田町1-65-1",
+            "電話番号":"0565-26-6777",
+            "営業時間開店":"全日　9:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"https://tempo.sega.jp/am/toyota",
+            "Twitter":"https://twitter.com/SEGA_toyota"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.937021,35.167471]
+          },
+          "properties":
+          {
+            "ID":"24",
+            "店舗名":"グレアハイパーアミューズメントタワー",
+            "郵便番号":"464-0850",
+            "住所":"愛知県名古屋市千種区今池5丁目22-17",
+            "電話番号":"052-731-2653",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"",
+            "Twitter":"https://twitter.com/grea_game"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[137.184167,35.0875]
+          },
+          "properties":
+          {
+            "ID":"25",
+            "店舗名":"プラサカプコン豊田店",
+            "郵便番号":"471-0014",
+            "住所":"愛知県豊田市東山町1-5-1 グリーンシティ 3F",
+            "電話番号":"0565-88-8020",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　20:00",
+            "Webサイト":"https://www.capcom.co.jp/amusement/game/shop/toyota/",
+            "Twitter":""
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.921758,35.161649]
+          },
+          "properties":
+          {
+            "ID":"26",
+            "店舗名":"スポルト名古屋店",
+            "郵便番号":"460-0007",
+            "住所":"愛知県名古屋市中区新栄2-45-26",
+            "電話番号":"052-261-3111",
+            "営業時間開店":"平日　10:00　土日祝　8:00",
+            "営業時間閉店":"平日　24:00　土日祝　24:00",
+            "Webサイト":"http://www.sport-bowling.co.jp/nagoya",
+            "Twitter":"https://twitter.com/nagoya06248545"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.904922,35.167923]
+          },
+          "properties":
+          {
+            "ID":"27",
+            "店舗名":"ゲームキングジョイ",
+            "郵便番号":"460-0008",
+            "住所":"名古屋市中区栄3-3-32 パチンコフジ2・3階",
+            "電話番号":"052-241-9515",
+            "営業時間開店":"全日　9:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"https://www.kingjoy.jp/",
+            "Twitter":"https://twitter.com/king_joy_"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.964249,35.137163]
+          },
+          "properties":
+          {
+            "ID":"28",
+            "店舗名":"ポート24八事店",
+            "郵便番号":"466-0834",
+            "住所":"愛知県名古屋市昭和区広路町北石坂102-4",
+            "電話番号":"052-834-9200",
+            "営業時間開店":"全日　9:00",
+            "営業時間閉店":"全日　24:00",
+            "Webサイト":"http://www.port24.co.jp/shop/yagoto",
+            "Twitter":"https://twitter.com/port24yagoto"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.949482,35.048944]
+          },
+          "properties":
+          {
+            "ID":"29",
+            "店舗名":"名古屋レジャーランド大高店",
+            "郵便番号":"459-8014",
+            "住所":"愛知県名古屋市緑区忠治山201",
+            "電話番号":"052-625-5360",
+            "営業時間開店":"24時間営業",
+            "営業時間閉店":"24時間営業",
+            "Webサイト":"https://www.leisurelan.co.jp/shop/ootaka.html",
+            "Twitter":"https://twitter.com/ll_oodaka"
+          }
+        },
+        {
+          "type":"Feature",
+          "geometry":
+          {
+            "type":"Point",
+            "coordinates":[136.974167,34.9788]
+          },
+          "properties":
+          {
+            "ID":"30",
+            "店舗名":"ユーズランド東浦店",
+            "郵便番号":"470-2102",
+            "住所":"愛知県知多郡東浦町大字緒川字旭13番2 イオンモール東浦2Ｆ",
+            "電話番号":"0562-82-2782",
+            "営業時間開店":"全日　10:00",
+            "営業時間閉店":"全日　21:00",
+            "Webサイト":"http://www.yous-am.com/higashiura.html",
+            "Twitter":"https://twitter.com/yous_higashiura"
+          }
+       },
+       {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[137.044999,35.21505]
+        },
+        "properties":
+        {
+          "ID":"31",
+          "店舗名":"Ｍ’ｓ ＰＡＲＫ 三郷店 西館",
+          "郵便番号":"480-0066",
+          "住所":"愛知県尾張旭市南原山町赤土207番地",
+          "電話番号":"0561-55-5103",
+          "営業時間開店":"全日　10:00",
+          "営業時間閉店":"全日　24:00",
+          "Webサイト":"http://todays-game.seesaa.net/article/397371955.html",
+          "Twitter":"https://twitter.com/ms_sangou_nishi"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[137.384578,34.802452]
+        },
+        "properties":
+        {
+          "ID":"32",
+          "店舗名":"豊川メトロポリス",
+          "郵便番号":"442-0824",
+          "住所":"愛知県豊川市下長山町上アライ14-1",
+          "電話番号":"0533-85-6540",
+          "営業時間開店":"平日　10:00　土日祝　9:00",
+          "営業時間閉店":"平日　23:50　土日祝　23:50",
+          "Webサイト":"http://www.korona.co.jp/game/tok/index.asp",
+          "Twitter":""
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.823828,35.125372]
+        },
+        "properties":
+        {
+          "ID":"33",
+          "店舗名":"中川メトロポリス",
+          "郵便番号":"454-0954",
+          "住所":"愛知県名古屋市中川区江松3-110",
+          "電話番号":"052-303-9701",
+          "営業時間開店":"平日　10:00　土日祝　9:00",
+          "営業時間閉店":"平日　23:50　土日祝　23:50",
+          "Webサイト":"http://www.korona.co.jp/game/nak/index.asp",
+          "Twitter":"https://twitter.com/nakagawametoro"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.912428,35.062989]
+        },
+        "properties":
+        {
+          "ID":"34",
+          "店舗名":"ゲームファンタジーランドナイル",
+          "郵便番号":"476-0002",
+          "住所":"愛知県東海市名和町三番割上40-2",
+          "電話番号":"052-601-7817",
+          "営業時間開店":"平日　11:00　土日祝　10:00",
+          "営業時間閉店":"平日　24:00　土日祝　24:00",
+          "Webサイト":"",
+          "Twitter":"https://twitter.com/nile7817"
+        }
+      },
+      {
+        "type":"Feature",
+        "geometry":
+        {
+          "type":"Point",
+          "coordinates":[136.787614,35.355722]
+        },
+        "properties":
+        {
+          "ID":"35",
+          "店舗名":"ゲームゾーンR　一宮店",
+          "郵便番号":"491-0134",
+          "住所":"愛知県一宮市更屋敷門60",
+          "電話番号":"",
+          "営業時間開店":"全日　9:00",
+          "営業時間閉店":"全日　24:00",
+          "Webサイト":"",
+          "Twitter":"https://twitter.com/gamezone_ichino"
+        }
+      }
+    ]
+}
